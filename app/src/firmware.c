54d07cfeedf72ef6855cc885b2c054bb68af278a
noobiexplorer <noobiexplorer@example.com>
2024-04-04 18:00:04 +0600

    Added ring buffer to UART driver for concurrency issues

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -16,58 +16,55 @@
 #define TX_PIN      GPIO2
 
 // Setting VTOR (Vector Table Offset Register)
-static void vector_setup(void)
-{
-    SCB_VTOR = BOOTLOADER_SIZE;
+static void vector_setup(void){
+  SCB_VTOR = BOOTLOADER_SIZE;
 }
 
 // Set up GPIO mode
-static void gpio_setup(void)
-{
-    rcc_periph_clock_enable(RCC_GPIOA);
-    // PWM gpio setup
-    gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
-    // PWM ch 1 is mapped to Alternate function 1
-    gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
-    
-    // UART gpio setup
-    gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, RX_PIN | TX_PIN);
-    gpio_set_af(UART_PORT, GPIO_AF7, TX_PIN | RX_PIN);
+static void gpio_setup(void){
+  rcc_periph_clock_enable(RCC_GPIOA);
+  // PWM gpio setup
+  gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
+  // PWM ch 1 is mapped to Alternate function 1
+  gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
+  
+  // UART gpio setup
+  gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, RX_PIN | TX_PIN);
+  gpio_set_af(UART_PORT, GPIO_AF7, TX_PIN | RX_PIN);
 }
 
 int main(void)
 {
-    vector_setup();
-    system_setup();
-    gpio_setup();
-    timer_setup();
-    uart_setup();
+  vector_setup();
+  system_setup();
+  gpio_setup();
+  timer_setup();
+  uart_setup();
 
-    uint64_t start_time = system_get_ticks();
-    float duty_cycle = 0.0f;
+  uint64_t start_time = system_get_ticks();
+  float duty_cycle = 0.0f;
 
-    timer_pwm_set_duty_cycle(duty_cycle);
+  timer_pwm_set_duty_cycle(duty_cycle);
 
-    while(1)
-    {
-        if (system_get_ticks() - start_time >= 30)
-        {
-            duty_cycle += 1.0f;
-            if (duty_cycle > 100.0f)
-            {
-                duty_cycle = 0.0f;
-            }
-            timer_pwm_set_duty_cycle(duty_cycle);
+  while(1){
+    if (system_get_ticks() - start_time >= 30){
+      duty_cycle += 1.0f;
+      if (duty_cycle > 100.0f){
+        duty_cycle = 0.0f;
+      }
+      timer_pwm_set_duty_cycle(duty_cycle);
 
-            start_time = system_get_ticks();
-        }
-        
-        if (uart_data_available()){
-            uint8_t data = uart_read_byte();
-            uart_write_byte(data + 1);
-        }
+      start_time = system_get_ticks();
     }
+      
+    if (uart_data_available()){
+      uint8_t data = uart_read_byte();
+      uart_write_byte(data + 1);
+    }
+    
+    system_delay(1000);
+  }
 
-    // Never return
-    return 0;
+  // Never return
+  return 0;
 }
--- shared/src/core/system.c
+++ shared/src/core/system.c
@@ -3,35 +3,39 @@
 #include <libopencm3/cm3/vector.h>
 #include <libopencm3/stm32/rcc.h>
 
+// Volatile: tells compiler to not optimize this var
+// and always update to the latest value
 static volatile uint64_t ticks = 0;
-void sys_tick_handler(void)
-{
-    ticks++;
+// Systick Interrupt handler
+void sys_tick_handler(void){
+  ticks++;
 }
 
-// Private functions - bound within this file
+// Private functions(Static) - bound within this file
 // Set up the internal clock oscillator
-static void rcc_setup(void)
-{
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
+static void rcc_setup(void){
+  rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
 }
 
-static void systick_setup(void)
-{
-    systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
-    systick_counter_enable();
-    systick_interrupt_enable();
+static void systick_setup(void){
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+  systick_counter_enable();
+  systick_interrupt_enable();
 }
 
 // Public functions
-uint64_t system_get_ticks(void)
-{
-    return ticks;
+uint64_t system_get_ticks(void){
+  return ticks;
 }
 
-void system_setup(void)
-{
-    rcc_setup();
-    systick_setup();
+void system_setup(void){
+  rcc_setup();
+  systick_setup();
 }
 
+void system_delay(uint64_t milliseconds){
+  uint64_t end_time = system_get_ticks() + milliseconds;
+  while(system_get_ticks() < end_time){
+    // Spin
+  }
+}
